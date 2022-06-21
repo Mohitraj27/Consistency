20937a2f0ee51aed505a9143a78c01bdf4c7a150
Mohit Raj <raj716980@gmail.com>
2022-06-21 13:05:14 +0530

    21/06/2022

---
+++ Day_1.java
@@ -0,0 +1,14 @@
+//sum of two numbers
+import java.util.*;
+class Day_1
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,sum=0;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        sum=a+b;
+        System.out.println("The sum of two numbers is "+sum);
+    }
+}
