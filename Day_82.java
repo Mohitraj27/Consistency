3c9323d161d02dd4e5d85cd7d3704337a93d40a5
Mohit Raj <raj716980@gmail.com>
2022-09-09 21:59:22 +0530

    Java Program to find the frequency of characters

---
+++ Day_82.java
@@ -0,0 +1,44 @@
+//Java Program to find the frequency of characters
+public class Day_82     
+{    
+     public static void main(String[] args) {    
+        String str = "picture perfect";    
+        int[] freq = new int[str.length()];    
+        int i, j;    
+            
+        //Converts given string into character array    
+        char string[] = str.toCharArray();    
+            
+        for(i = 0; i <str.length(); i++) {    
+            freq[i] = 1;    
+            for(j = i+1; j <str.length(); j++) {    
+                if(string[i] == string[j]) {    
+                    freq[i]++;    
+                        
+                    //Set string[j] to 0 to avoid printing visited character    
+                    string[j] = '0';    
+                }    
+            }    
+        }    
+            
+        //Displays the each character and their corresponding frequency    
+        System.out.println("Characters and their corresponding frequencies");    
+        for(i = 0; i <freq.length; i++) {    
+            if(string[i] != ' ' && string[i] != '0')    
+                System.out.println(string[i] + "-" + freq[i]);    
+        }    
+    }    
+}    
+//Output
+/*
+ Characters and their corresponding frequencies
+p-2
+i-1
+c-2
+t-2
+u-1
+r-2
+e-3
+f-1 
+
+ */
