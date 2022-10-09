99259cda96478f626dfe99e0d4cc5df33b758919
Mohit Raj <raj716980@gmail.com>
2022-10-09 22:58:13 +0530

    Program to replace the second element of a Array List with the specified element.

---
+++ Day_112.java
@@ -0,0 +1,26 @@
+//Program to replace the second element of a ArrayList with the specified element.
+import java.util.ArrayList;
+  public class Day_112 {
+    public static void main(String[] args){
+  ArrayList<String>  color = new ArrayList<String>();
+
+  color.add("Red");
+  color.add("Green");
+
+  System.out.println("Original array list: " + color);
+  String new_color = "White";
+  color.set(1,new_color);
+
+  int num=color.size();
+  System.out.println("Replace second element with 'White'."); 
+  for(int i=0;i<num;i++)
+  System.out.println(color.get(i));
+  }
+}
+/* Output of the Program
+ Original array list: [Red, Green]
+Replace second element with 'White'.
+Red
+White
+ */
+
