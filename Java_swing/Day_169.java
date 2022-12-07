d2c220914c8b831c02dd1edb2bfca48136975d9b
Mohit Raj <raj716980@gmail.com>
2022-12-07 21:57:58 +0530

    Java Editor Pane

---
+++ Java_swing/Day_169.java
@@ -0,0 +1,23 @@
+import javax.swing.JEditorPane;  
+import javax.swing.JFrame;  
+  
+public class Day_169 {  
+    JFrame myFrame = null;  
+  
+    public static void main(String[] a) {  
+        (new Day_169()).test();  
+    }  
+  
+    private void test() {  
+        myFrame = new JFrame("JEditorPane Test");  
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        myFrame.setSize(400, 200);  
+        JEditorPane myPane = new JEditorPane();  
+        myPane.setContentType("text/plain");  
+        myPane.setText("Sleeping is necessary for a healthy body."  
+                + " But sleeping in unnecessary times may spoil our health, wealth and studies."  
+                + " Doctors advise that the sleeping at improper timings may lead for obesity during the students days.");  
+        myFrame.setContentPane(myPane);  
+        myFrame.setVisible(true);  
+    }  
+}  
