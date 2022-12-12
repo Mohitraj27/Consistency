6a7ff5fd94ae021909eb4b1dac5cdbcf0c0ad90f
Mohit Raj <raj716980@gmail.com>
2022-12-12 23:30:57 +0530

    Add files via upload

---
+++ Java_swing/Day_174.java
@@ -0,0 +1,14 @@
+import java.awt.*;     
+class Day_174 {     
+Day_174(){     
+Frame f=new Frame();     
+Image icon = Toolkit.getDefaultToolkit().getImage("D:\\icon.png");    
+f.setIconImage(icon);    
+f.setLayout(null);     
+f.setSize(400,400);     
+f.setVisible(true);     
+}     
+public static void main(String args[]){     
+new Day_173();     
+}     
+}    
