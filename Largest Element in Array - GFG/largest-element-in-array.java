e4c369b70bbf8d55bdc8143272f01de6a4ba2cb3
Mohit Raj <raj716980@gmail.com>
2023-02-05 12:33:34 +0530

    Added solution - LeetHub

---
+++ Largest Element in Array - GFG/largest-element-in-array.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Compute obj = new Compute();
+            System.out.println(obj.largest(a, n));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Compute {
+    
+    public int largest(int arr[], int n)
+    {
+        Arrays.sort(arr);
+        return arr[n-1];
+        
+        
+    }
+}
