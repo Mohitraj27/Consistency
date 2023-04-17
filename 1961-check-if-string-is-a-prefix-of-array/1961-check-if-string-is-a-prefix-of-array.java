20abc6b83af0e21f0b88061ead0f51aea934e414
Mohit Raj <raj716980@gmail.com>
2023-04-17 17:16:36 +0530

    Time: 1 ms (95.83%), Space: 42.7 MB (23.03%) - LeetHub

---
+++ 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPrefixString(String s, String[] words) {
+        
+        String res="";
+        //Here we'r iterrating through each word of the arrays words
+        for(int i=0;i< words.length;i++)
+        {
+            // res string variable is intilalized with empty space and it
+            // is compared with s 
+            res+=words[i];
+            if(res.equals(s))
+            {
+                return true;
+            }
+         
+        }
+        return false;
+        
+    }
+    
+}
