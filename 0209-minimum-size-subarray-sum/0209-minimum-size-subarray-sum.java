063b80af5a76f04238c80d3b1c72b0a59684f8d5
Mohit Raj <raj716980@gmail.com>
2023-05-24 23:51:27 +0530

    Time: 1 ms (100.00%), Space: 54.3 MB (5.03%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -4,23 +4,26 @@ class Solution {
         int ans=Integer.MAX_VALUE;
         
         int sum=0;
-        
+        // left index counter 
             int left=0;
         for(int i=0;i<nums.length;i++)
         {
             sum+=nums[i];
             
             while(sum>=target){
+                // i+1-left will remove the remaining  left  elements  and storing the minimum index among them 
+                
                 ans=Math.min(ans,i+1-left);
                 sum-=nums[left];
                 left++;
             }
         }
-        //If there is no such array then return 0
-        if(ans==Integer.MAX_VALUE){
-            return 0;
-        }
-        else{
-    return ans;}
+       return ans=(ans==Integer.MAX_VALUE)?0:ans;
+    //     //If there is no such array then return 0
+    //     if(ans==Integer.MAX_VALUE){
+    //         return 0;
+    //     }
+    //     else{
+    // return ans;}
     }
 }
