f0cb9fc9e8aa0f5de4d26a15123b11dc6091fd77
Mohit Raj <raj716980@gmail.com>
2023-05-24 22:49:21 +0530

    Added solution - LeetHub

--- Chocolate Distribution Problem - GFG/chocolate-distribution-problem.java
+++ Chocolate Distribution Problem - GFG/chocolate-distribution-problem.java
@@ -38,16 +38,27 @@ class Solution
 {
     public long findMinDiff (ArrayList<Integer> a, int n, int m)
     {
+        //Using the concept of sliding window
         //cause we need the minimum gap
+        
         int ans=Integer.MAX_VALUE;
         // your code here
+        //Sorted using java collection framework cause Arrays.sort won't work for arraylist
         Collections.sort(a);
+        //Iterating through each element of array
         for(int i=0;i<=n-m;i++)
-        {
+        { //Intializing minimum window to first element
             int minwindow=a.get(i);
+            //taking last element for a sliding window
             int maxwindow=a.get(i+m-1);
+            // for that selected window caluclating the gap
             int gap=maxwindow-minwindow;
+        //conditon for comparing the gap with ans 
+        // in ans variable max integer value is stored so that it should get comapred 
+        // after each iteration and after if gap is minimum then the previous window if condition 
+        // statisfies and ans will stored with that minimum gap value /
         
+        // then ans will be 
         if(gap<ans)
         {
             ans =gap;
@@ -55,6 +66,7 @@ class Solution
         
         
         }
+        // after final iteration is completed ans is returned
         
         return ans;
     }
