1483ee969ae398fb6ac2184d44a903263c6655b4
Mohit Raj <raj716980@gmail.com>
2023-05-31 23:57:24 +0530

    Added solution - LeetHub

---
+++ Merge Sort for Linked List - GFG/merge-sort-for-linked-list.java
@@ -0,0 +1,104 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Driverclass
+{
+    
+    public static void main (String[] args) 
+    {
+        Scanner sc= new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            while(n-- > 1){
+		        tail.next = new Node(sc.nextInt());
+		        tail = tail.next;
+		    }
+		   
+		      head = new Solution().mergeSort(head);
+		     printList(head);
+		    System.out.println();
+        }
+    }
+    public static void printList(Node head)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        this.data = key;
+        next = null;
+    }
+} */
+
+class Solution
+{
+    //Function to sort the given linked list using Merge Sort.
+    static Node mergeSort(Node head)
+    {
+          int n = 0;
+        for(Node curr = head;curr!=null;curr = curr.next)
+        {
+            n++;
+        }
+        int arr[] = new int[n];
+        int i=0;
+        for(Node curr = head;curr!=null;curr = curr.next)
+        {
+            arr[i] = curr.data;
+            i++;
+        }
+        Arrays.sort(arr);
+        i=0;
+        for(Node curr = head;curr!=null;curr = curr.next)
+        {
+            curr.data = arr[i];
+            i++;
+        }
+        return head;
+    }
+        
+    
+}
+
+
