973d666e6c6138c1d499121511933c4488423f95
Mohit Raj <raj716980@gmail.com>
2023-06-05 23:34:14 +0530

    Added solution - LeetHub

--- Find Pair Given Difference - GFG/find-pair-given-difference.java
+++ Find Pair Given Difference - GFG/find-pair-given-difference.java
@@ -42,16 +42,14 @@ class Solution
     public boolean findPair(int arr[], int size, int n)
     {
         //code here.
-        Arrays.sort(arr); 
+       // Arrays.sort(arr); 
         // time - O(n^2) brute force approach 
-        for (int i = 0; i < size - 1; i++) {
-        for (int j = i + 1; j < size; j++) {
-            int diff = arr[j] - arr[i];
+        for (int i = 0; i < size-1 ; i++) {
+        for (int j = i+1; j < size; j++) {
+            int diff = Math.abs(arr[j] - arr[i]);
             if (diff == n) {
                 return true;
-            } else if (diff > n) {
-                break;
-            }
+            } 
         }
     }
         
