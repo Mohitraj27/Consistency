32d87d1d0c12ec05997425b2ace0c468063d1cfe
Mohit Raj <raj716980@gmail.com>
2023-06-07 20:44:57 +0530

    Added solution - LeetHub

---
+++ K-th element of two sorted Arrays - GFG/kth-element-of-two-sorted-arrays.java
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            int n = Integer.parseInt(stt.nextToken());
+            int m = Integer.parseInt(stt.nextToken());
+            int k = Integer.parseInt(stt.nextToken());
+            int a[] = new int[(int)(n)];
+            int b[] = new int[(int)(m)];
+            
+            
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < m; i++) {
+                b[i] = Integer.parseInt(inputLine1[i]);
+            }
+            
+            
+            Solution obj = new Solution();
+            System.out.println(obj.kthElement( a, b, n, m, k));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    public long kthElement( int nums1[], int nums2[], int n, int m, int k) {
+        
+        
+        int i=0,j=0,kth=0;
+        while(i<n && j<m){
+            if(nums1[i]<nums2[j]){
+                kth++;
+                if(kth==k){
+                    return nums1[i];
+                }
+                i++;
+            }
+            else{
+                kth++;
+                if(kth==k){
+                    return nums2[j];
+                }
+                j++; 
+            }
+        }
+        
+        while(i<n)
+        {
+             kth++;
+                if(kth==k){
+                    return nums1[i];
+                }
+                i++;
+        }
+        while(j<m)
+        {
+             kth++;
+                if(kth==k){
+                    return nums2[j];
+                }
+                j++;
+        }
+        return -1;
+    }
+}
