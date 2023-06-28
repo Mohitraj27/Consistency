836a016dc4e2d190dc42bd548dfdb7545b71ed29
Mohit Raj <raj716980@gmail.com>
2023-06-28 21:08:23 +0530

    Added solution - LeetHub

---
+++ Preorder Traversal - GFG/preorder-traversal.java
@@ -0,0 +1,151 @@
+//{ Driver Code Starts
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+                ArrayList<Integer> res = BinaryTree.preorder(root);
+                for (Integer num : res)
+                    System.out.print(num + " ");
+                System.out.println();
+                t--;
+            
+        }
+    }
+   
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+// class Node{
+//     int data;
+//     Node left,right;
+//     Node(int d){
+//         data=d;
+//         left=right=null;
+//     }
+// }
+
+class BinaryTree
+{
+    //Function to return a list containing the preorder traversal of the tree.
+    static ArrayList<Integer> preorder(Node root)
+    {
+        // Code here
+        /*recursive approach Preorder works on +LR 
+        + -> root node
+        L -> left node
+        R -> right node
+        1. Visit the root node
+        2. Traverse the left subtree in Pre-order fashion
+        3. Traverse the rigth subtree in pre-order fashion
+        */
+        ArrayList<Integer> result=new ArrayList<>();
+        if(root==null){
+            return result;
+        }
+        // first returning the rooot ndoe  and then left and so right again recursively 
+        // work for other root node
+        result.add(root.data);
+        result.addAll(preorder(root.left));
+        result.addAll(preorder(root.right));
+        
+        
+        return result;
+    }
+
+}
