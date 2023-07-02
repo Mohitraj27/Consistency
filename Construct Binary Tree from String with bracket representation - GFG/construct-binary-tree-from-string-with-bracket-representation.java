564e2944c00385c50d421c977d4ccffe48a5458a
Mohit Raj <raj716980@gmail.com>
2023-07-02 22:54:58 +0530

    Added solution - LeetHub

---
+++ Construct Binary Tree from String with bracket representation - GFG/construct-binary-tree-from-string-with-bracket-representation.java
@@ -0,0 +1,171 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+    
+    public static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length()==0 || str.charAt(0)=='N')
+            return null;
+        
+        // Creating array of Strings from input 
+        // String after spliting by space
+        String ip[] = str.split(" ");
+        
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        
+        // Push the root to the queue
+        Queue<Node> queue = new LinkedList<>(); 
+        queue.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(queue.size()>0 && i < ip.length)
+        {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N"))
+            {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N"))
+            {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+    public static Node inputTree(BufferedReader br) throws IOException
+    {
+        return buildTree(br.readLine().trim());
+    }
+    
+    public static void inorder(Node root)
+    {
+        if (root == null)
+           return;
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            String s; 
+            s = br.readLine();
+            
+            Solution obj = new Solution();
+            Node res = obj.treeFromString(s);
+            
+            Node.inorder(res);
+            System.out.println();
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+
+Definition for Binary Tree Node
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+class Solution {
+    public static Node treeFromString(String s) {
+        // code here
+        int n = s.length();
+        return f(s,0,n);
+    }
+    
+    public static Node f(String s,int lo,int hi){
+        if(lo>=hi) return null;
+        int val = 0;
+        int mul = 10;
+        int i=lo;
+        
+        while(i<hi && s.charAt(i)!='(' && s.charAt(i)!=')'){
+            val = val*10 + (s.charAt(i)-'0');
+            i++;
+        }
+        
+        int start = i;
+        Node root = new Node(val);
+        if(i==hi) return root;
+        i++;
+        int open = 1;
+        
+        while(i<hi && open!=0){
+            if(s.charAt(i)=='(') open++;
+            else if(s.charAt(i)==')') open--;
+            i++;
+        }
+        
+        root.left = f(s,start+1,i-1);
+        if(i==hi) return root;
+        root.right = f(s,i+1,hi-1);
+        return root;
+    }
+}
