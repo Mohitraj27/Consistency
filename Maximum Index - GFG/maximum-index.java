7b5b793254feacd489a292ad0bc0f60a948d4693
Mohit Raj <raj716980@gmail.com>
2023-07-03 00:22:36 +0530

    Added solution - LeetHub

---
+++ Maximum Index - GFG/maximum-index.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            System.out.println(new Solution().maxIndexDiff(arr, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+class Solution {
+
+    int maxIndexDiff(int arr[], int n) {
+        int[] leftMin = new int[n];
+        int[] rightMax = new int[n];
+
+        leftMin[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            leftMin[i] = Math.min(arr[i], leftMin[i - 1]);
+        }
+
+        rightMax[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(arr[i], rightMax[i + 1]);
+        }
+
+        int maxDiff = 0;
+        int i = 0, j = 0;
+        while (i < n && j < n) {
+            if (leftMin[i] <= rightMax[j]) {
+                maxDiff = Math.max(maxDiff, j - i);
+                j++;
+            } else {
+                i++;
+            }
+        }
+
+        return maxDiff;
+    }
+}
