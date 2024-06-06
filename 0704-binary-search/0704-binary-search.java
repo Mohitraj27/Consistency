d8b67f49748b4e5b3fe6edbeb48422ee83819774
Mohit Raj <raj716980@gmail.com>
2024-06-06 23:34:28 +0530

    Time: 0 ms (100.00%), Space: 45.6 MB (37.41%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,36 +1,20 @@
 class Solution {
-    public int search(int[] nums, int target) {
+    public int search(int[] arr, int target) {
+        int low=0,high=arr.length-1;
         
-        //Applting Binary search algorithm
-        //Intialize the starting element to 0 and ending element to length of arrat
-          int start=0;
-        
-       int end=nums.length-1;
-        
-       while(start<=end)
-       {
-           //Extracting the middle value of a array
-           int mid=(start+end)/2;
-           
-           if(nums[mid]==target)
-               return mid;
-           
-           else if(nums[mid]<target){
-           
-               start=mid+1;
-           }else{
-               end=mid-1;
-           }
-       }
-        //using basic linear search technique
-//         for(int i=0;i<nums.length;i++)
-//         {
-//             if(nums[i]==target)
-//             {       return i;
-//         }   
-       
-//     } 
-           
-        return -1;
-}
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[mid]<target){
+                low=mid+1;
+                
+            }
+            else{
+                high=mid-1;
+            }
+        } return -1;
+    }
 }
