cf5dd40edbdad8c94d7030c2379784934aaccfec
Mohitraj27 <raj716980@gmail.com>
2024-08-18 22:38:49 +0530

    Factorial

---
+++ Javascript/Basics/18_Aug_2024.js
@@ -0,0 +1,11 @@
+// Factorial of a number
+
+function factorial(number) {
+  if (number === 0 || number === 1) {
+    return 1;
+  } else {
+    return number * factorial(number - 1);
+  }
+}
+
+console.log(factorial(5));
