1a7e891465020a7b23a7a56cf2e3b1e0cbf3938a
Mohitraj27 <raj716980@gmail.com>
2024-08-27 22:34:38 +0530

    27_Aug_2024

---
+++ Javascript/Basics/27_Aug_2024.js
@@ -0,0 +1,19 @@
+// fetch data from public api
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = 3000;
+
+app.get('/api/data', async (req,res)=>{
+    try{
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        res.json(response.data);
+    }catch(error){
+        console.error('Error fetching data',error);
+        res.status(500).json({error:'Failed to fetch data'});
+    }
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on PORT ${PORT}`);
+})
