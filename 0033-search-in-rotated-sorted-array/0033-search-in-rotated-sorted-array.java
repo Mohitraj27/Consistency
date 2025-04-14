546c921e840ecb1bae9e5e24767af27ca0fd7c31
Mohit Raj <raj716980@gmail.com>
2025-04-14 21:53:16 +0530

    Time: 0 ms (100%), Space: 42.7 MB (6.73%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,15 +1,24 @@
 class Solution {
     public int search(int[] arr,int target) {
-        /*
-        Brute Force Approach
-        Time - O(N)
+        /* Using Two Ptr Approach 
+        Time - O(N/2)
+        Space - O(1)
+        1. Intialize left pointer to zeroth index and right ptr to last index
+        2. Itertae throughout the array until left ptr <= right ptr
+        3. add two check if the right or left ptr index element matches with the target element 
+        then return tht element else return -1;
+        
         */
-        int i=0;
-        while(i<=arr.length-1){
-            if(arr[i]==target){
-                return i;
+        int left=0,right=arr.length-1;
+        while(left<=right){
+            if(arr[left]==target){
+                return left;
             }
-            i++;
+            if(arr[right]==target){
+                return right;
+            }
+            left++;
+            right--;
         }
         return -1;
     }
