9fdbbd6bc36e1e02e58635fd08ba3a2647131d6a
Mohit Raj <raj716980@gmail.com>
2025-06-11 17:36:36 +0530

    Time: 2 ms (57.26%), Space: 41.4 MB (47.31%) - LeetHub

--- 1848-sum-of-unique-elements/1848-sum-of-unique-elements.java
+++ 1848-sum-of-unique-elements/1848-sum-of-unique-elements.java
@@ -1,20 +1,35 @@
 class Solution {
     public int sumOfUnique(int[] nums) {
-        Map<Integer,Integer> map=new HashMap();
-        int res = 0;
-        for(int i:nums){
-            int f = map.getOrDefault(i,-1);
-            if(f == -1){
-                res += i;
-                map.put(i,1);
-            }else if(f !=-1 && f != 0){
-                res -= i;
-                map.put(i,0);
+        // Map<Integer,Integer> map=new HashMap();
+        // int res = 0;
+        // for(int i:nums){
+        //     int f = map.getOrDefault(i,-1);
+        //     if(f == -1){
+        //         res += i;
+        //         map.put(i,1);
+        //     }else if(f !=-1 && f != 0){
+        //         res -= i;
+        //         map.put(i,0);
+        //     }
+        // }
+        // return res;
+         int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            boolean isUnique = true;
+            for (int j = 0; j < nums.length; j++) {
+                if (i != j && nums[i] == nums[j]) {
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique) {
+                res += nums[i];
             }
         }
         return res;
     }
 }
+
 
 /**
     Step 1: Intaialize one Hash Map and Iterate through each element
@@ -22,4 +37,6 @@ class Solution {
     Step 3. If any duplicates element found then remove that element from the map and also subtract the element from total sum
     Step 4: return the final sum after performing the operation of sum of unique elements
     time - O(N) Space O(N)
+
+    can also be solved through brute force approach O(N^2) Space O(N) 
  */
