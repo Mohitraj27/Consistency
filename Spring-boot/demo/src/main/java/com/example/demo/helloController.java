769809fb48cb4ba95e42f7e6c28933f866f24d8b
Mohitraj27 <raj716980@gmail.com>
2025-07-22 21:48:09 +0530

    First Spring Boot Program

---
+++ Spring-boot/demo/src/main/java/com/example/demo/helloController.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
