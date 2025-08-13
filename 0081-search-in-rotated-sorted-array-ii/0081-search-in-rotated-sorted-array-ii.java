2c87d84d0d4665c86d2ea441ebf27e0ad25fdaf8
Mohit Raj <raj716980@gmail.com>
2025-08-13 13:09:39 +0530

    Time: 0 ms (100%), Space: 43.8 MB (10.66%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,17 +1,31 @@
 class Solution {
-    public boolean search(int[] nums, int target) {
+    public boolean search(int[] arr, int target) {
+        int low =0,high = arr.length-1;
         
-        
-        //Same approach as that of Search in Sorted array using linear seach technique 
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]==target)
-            {
-                return true;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+
+            if(arr[mid]==target) return true;
+
+            // Only changes to handle the duplicates inside the given array
+            if(arr[low]==arr[mid] && arr[mid]==arr[high]){
+                low++;
+                high--;
+            }else if(arr[low]<=arr[mid]){
+                if (arr[low] <= target && target < arr[mid]) { 
+                    high = mid - 1; 
+                } else {
+                    low = mid + 1;
+                }
+            } else{
+                    if(arr[mid]<=target && target <=arr[high]){
+                        low = mid+1;
+                    }else{
+                    
+                        high = mid-1;
+                    }
             }
         }
         return false;
-        
-        
     }
 }
