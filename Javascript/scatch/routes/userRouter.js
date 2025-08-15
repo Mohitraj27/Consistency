d493caf3e3da3b8048f720eba363722ba0125480
Mohitraj27 <raj716980@gmail.com>
2025-08-15 23:19:44 +0530

    Sockets Implementation

--- Javascript/scatch/routes/userRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', function(req, res) {
-    res.send("hey this is the products route");
-});
-
-module.exports = router; 
