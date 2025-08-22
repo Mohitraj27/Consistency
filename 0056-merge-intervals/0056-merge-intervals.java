bc63b3f2348c8a0d3a1ee0f2f259dc4bfd8b2595
Mohit Raj <raj716980@gmail.com>
2025-08-22 21:29:48 +0530

    Time: 9 ms (44.34%), Space: 45.6 MB (99.99%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,32 +1,41 @@
 class Solution {
-    public int[][] merge(int[][] intervals) {
+public int[][] merge(int[][] intervals) {
    
         //First sorted the array list using first pair of x element
         
+    /*
+    below is the lamba expression that compares two intervals a and b by their 
+    first elements (a[0],b[0]) using comapare() methof to perform the comparison.
+    this methd returns a -ve value if(a[0]<b[0]) 
+    */
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        
-        //a new st stack is created
-        Stack<Integer> st=new Stack<Integer>();
-        
-        st.push(intervals[0][0]);
-        st.push(intervals[0][1]);
-        for(int i=1;i<intervals.length;i++){
-            int x=st.pop();
-            if(intervals[i][0]<=x){
-                st.push(Math.max(intervals[i][1],x));
-            }
-            else{
-                st.push(x);
-                st.push(intervals[i][0]);
-                st.push(intervals[i][1]);
-            }
+ 
+    // Create a new ArrayList 
+    ArrayList<int[]> mergedIntervals = new ArrayList<>();
+
+    mergedIntervals.add(intervals[0]);
+
+    for (int i = 1; i < intervals.length; i++) {
+        int[] currentInterval = intervals[i];
+        int[] previousInterval = mergedIntervals.get(mergedIntervals.size() - 1);
+
+        if (currentInterval[0] <= previousInterval[1]) {
+            // Merge the intervals
+            previousInterval[1] = Math.max(currentInterval[1], previousInterval[1]);
+        } else {
+            // Add the current interval to the merged intervals list
+            mergedIntervals.add(currentInterval);
         }
-        int[][] ans=new int[st.size()/2][2];
-        for(int i=st.size()/2-1;i>=0;i--){
-            for(int j=1;j>=0;j--){
-                ans[i][j]=st.pop();
-            }
-        }
-        return ans;
     }
-} 
+
+    // Convert the ArrayList to a 2D array
+    int[][] mergedArray = new int[mergedIntervals.size()][2];
+    for (int i = 0; i < mergedIntervals.size(); i++) {
+        mergedArray[i] = mergedIntervals.get(i);
+    }
+
+    return mergedArray;
+}
+
+    
+}
