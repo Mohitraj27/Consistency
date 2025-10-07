f524e93ac84b49363fbda81f5d280655286e16d8
Mohit Raj <raj716980@gmail.com>
2025-10-07 13:18:16 +0530

    Time: 2 ms (73.09%), Space: 61.1 MB (95.12%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,19 +1,25 @@
 class Solution {
-    public int maxProfit(int[] arr) {
-        int min = Integer.MAX_VALUE;
-        int len = arr.length;
-        int max_profit =0;
-        for(int i=0;i<len;i++){
-            if(arr[i]<min){
-                min = arr[i];
-            }else if(arr[i]-min >max_profit){
-                max_profit = arr[i]-min;
-            }
+     
+     public int maxProfit(int[] prices) {
+       //for selling at maximum profit
+        int max_profit=0;
+        
+        //purchasing at mininum price 
+       int  min_price=Integer.MAX_VALUE;//Math.max(prices[0]);
+        //for iterating through each single  days 
+        for(int i=0;i<prices.length;i++)
+        { 
+            // i represents the no of days
+            
+           //if(prices[i+1]-prices[i]>max_prof) 
+            // max_prof=prices[i+1]-prices[i];
+            // return max_prof;
+            
+        min_price=Math.min(prices[i],min_price);
+            max_profit=Math.max(max_profit,prices[i]-min_price);
+        
         }
         return max_profit;
-
-
-        
+    
     }
 }
-
