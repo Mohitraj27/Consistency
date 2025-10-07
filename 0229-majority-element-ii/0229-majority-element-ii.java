4c0754c89d87afb25015477caf8931b3749c6f52
Mohit Raj <raj716980@gmail.com>
2025-10-07 13:20:04 +0530

    Time: 2735 ms (5.04%), Space: 46.8 MB (99.9%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,77 +1,28 @@
 class Solution {
-    public static List<Integer> majorityElement(int[] nums) {
+    public List<Integer> majorityElement(int[] nums) {
+         int n=nums.length;
+        // majority element should be n/3
+        int majoritycount=nums.length/3;
         
-        //Using this Burte force approach 
-        /* At first Create an array List to store the list of numbers
-         we check each element of an array 
-         and intialize count to 1 
-         if any element of an array is similar to its next element 
-         increment the counter variable to ++
-         and if the count is greater than n/3 or (n/kth ) element
-         add it to the arraylist created after iterating throughout the array 
-         return the arrayList
-//         */
-//         ArrayList <Integer> ans=new ArrayList<> ();
-        
-//         for(int  i=0;i<nums.length;i++)
-//         {
-//             int count=1;
-//             for(int j=i+1;j<nums.length;j++)
-//             {
-                
-//                 if(nums[i]==nums[j])
-                
-//                     count++;
-//             }
-//           if(count>(nums.length/3))
-        
-//             ans.add(nums[i]);
-//         }
-//                 return ans;
-    
-    int number1=-1,number2=-1,count1=0,count2=0, len=nums.length;
-        
-        for(int i=0;i<len;i++)
+        // Using HashSet
+        Set<Integer> result=new HashSet<>();
+        //Nested for loop
+        for(int i=0;i<n;i++)
         {
-            if(nums[i]==number1)
-                count1++;
-            else if(nums[i]==number2)
-                count2++;
-            else if(count1==0){
-                number1=nums[i];
-            count1=1;
-        }
-        else if(count2==0)
-        {
-            number2=nums[i];
-            count2=1;
-            
-        }
-            else {
-                count1--;
-                count2--;
+            //counting the elements which are more than one 
+            int count=0;
+            for(int j=0;j<n;j++)
+            {
+                if(nums[i]==nums[j])
+                {  count++;
             }
+            }
+        // if that element are more n/3 add it to the hashset
+        if(count>majoritycount){
+            result.add(nums[i]);
         }
-    
-        
-        ArrayList <Integer> ans=new ArrayList<Integer>();
-        count1=0;
-        count2=0;
-        for(int i=0;i<len;i++)
-        {
-            if(nums[i]==number1)
-                count1++;
-            else if(nums[i]==number2)
-                count2++;
         }
-        if (count1 > len / 3)
-      ans.add(number1);
-    if (count2 > len / 3)
-      ans.add(number2);
-    return ans;
+        //return the hashset in form of arraylist cause fucntion is in form of list
+        return new ArrayList<>(result);
     }
-        
-        
-      
-    
 }
