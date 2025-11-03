7bafc17da2c3ff42772f11cfe04045aed18a4922
Mohit Raj <raj716980@gmail.com>
2025-11-03 18:32:35 +0530

    Time: 870 ms (72.53%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,4 +1,2 @@
 # Write your MySQL query statement below
-SELECT name, bonus
-FROM Employee LEFT JOIN Bonus USING(empId)
-WHERE COALESCE(bonus, 0) < 1000; 
+select e.name ,b.bonus from employee as e left join Bonus as b on e.empId=b.empId where b.bonus <1000 or b.bonus is null;
