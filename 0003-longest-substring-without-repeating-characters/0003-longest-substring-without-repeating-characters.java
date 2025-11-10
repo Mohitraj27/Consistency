699b32578f96842e1c2ef6206710be64d592f373
Mohit Raj <raj716980@gmail.com>
2025-11-10 20:22:44 +0530

    Time: 5 ms (87.39%), Space: 43.9 MB (89.15%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,41 +1,22 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+       int start=0,len=0;
+        HashMap<Character, Integer> map=new HashMap<>();
         
-        //Optimal Solution using two pointer approach
-        int left_pointer=0,right_pointer=0;
-        
-        //This Hashset will help to check for the duplication & repition of characters
-        Set<Character> seen=new HashSet();
-        
-        //For counting the longest substring 
-        int longest_substring=0;
-        while(right_pointer<s.length())
-        {
-            char c=s.charAt(right_pointer);
-            if(seen.add(c))
+        for(int end=0;end<s.length();end++){
+            char c=s.charAt(end);
+            
+            if(map.containsKey(c))
             {
-                longest_substring=Math.max(longest_substring,right_pointer-left_pointer+1);
-                right_pointer++;
-                
+                if(start<=map.get(c))
+                {
+                    start=map.get(c)+1;
+                }
             }
-            else
-                //for duplication of characters
-            {
-                while(s.charAt(left_pointer)!=c)
- 
-            { 
-                    //This while loop will work until the left character is removed
-                    seen.remove(s.charAt(left_pointer));
-                left_pointer++;
-            }
-            //Below logic will work if there is duplication of character
-            seen.remove(c);
-            left_pointer++;
-            }
+            
+            len=Math.max(len,end-start+1);
+            map.put(c,end);
         }
-        
-        return longest_substring;
-        }
-        
-        
+        return len;
     }
+}
