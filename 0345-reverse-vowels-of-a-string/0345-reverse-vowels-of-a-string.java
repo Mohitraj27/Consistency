2f94e9f18b52fe427c619e61ad3e5df6936f01ee
Mohit Raj <raj716980@gmail.com>
2026-01-09 12:12:15 +0530

    Time: 2 ms (99.24%), Space: 46.6 MB (55.32%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,41 +1,32 @@
 class Solution {
+   
     public String reverseVowels(String s) {
-        /**
-            Two ptrs start and end
-            Intialize start to zeroth index and end to last Index of array
-            Convert all the characters of a string to a CharArray
-            Iterate throughout the String check through each character using
-            start and end ptr are not Vowel then increment the start ptr by 1 
-            and decrement ptr by 1 else if both are vowel then swap both the 
-            vowels
-            Time O(N) Space O(N) 
-            
-         */
-        int n = s.length();
-        char ch[] = s.toCharArray();
-        int start = 0;
-        int end = n-1;
-        while(start < end){
-            if(!isVowel(ch[start])){
-                start++;
-            }else if(!isVowel(ch[end])){
-                end--;
-            }else{
-                char temp = ch[start];
-                ch[start] = ch[end];
-                ch[end]= temp;
-                start++;
-                end--;
+        char[] ch = s.toCharArray();
+        int left = 0, right = ch.length - 1;
+
+        while (left < right) {
+
+            while (left < right && !isVowel(ch[left])) {
+                left++;
             }
+
+            while (left < right && !isVowel(ch[right])) {
+                right--;
+            }
+
+            char temp = ch[left];
+            ch[left] = ch[right];
+            ch[right] = temp;
+
+            left++;
+            right--;
         }
-        return String.valueOf(ch);
+
+        return new String(ch);
     }
 
-    public static boolean isVowel(char ch){
-        if(ch =='a'||ch=='e'||ch=='i'|| ch=='o'|| ch=='u'||ch =='A'||ch=='E'||ch=='I'|| ch=='O'|| ch=='U'){
-            return true;
-        }else{
-            return false;
-        }
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+            || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
     }
 }
